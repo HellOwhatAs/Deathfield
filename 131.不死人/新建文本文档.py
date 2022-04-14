47613348133952b738f5e991476cb5320d8f062e
HellOwhatAs <88815487+HellOwhatAs@users.noreply.github.com>
2022-04-14 17:41:36 +0800

    Delete 新建文本文档.py

--- 131.不死人/新建文本文档.py
+++
@@ -1,8 +0,0 @@
-a=[]
-s,e=10,14
-for i in range(s,e):
-    with open("{}.jpg".format(i),"rb") as f:
-        a.append(f.read())
-for iii,i in enumerate(range(s,e)):
-    with open("{}.jpg".format(i-1),"wb") as f:
-        f.write(a[iii])
